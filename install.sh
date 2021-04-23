6f6f16374070fefe16207e141a4dc209be794de0
Rada Berar <ujagaga@gmail.com>
2021-04-23 06:41:20 +0200

    Updated install

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 INSTALL_DIR="/opt/cmdserver"
 PROJECT_NAME=command-server
 GIT_URL=https://github.com/ujagaga/$PROJECT_NAME.git
-GIT_BRANCH=master
+GIT_BRANCH=main
 TEMP_DIR="/tmp"
 UPDATES_FILE="update.info"
 APP_NAME=bnfs
-SRC_DIR=src
+SRC_DIR=.
 USB_MOUNT_PACKET=usbmount_0.0.24_all.deb
 LOGFILE="install.log"
 
@@ -61,13 +61,24 @@ git clone $GIT_URL
 cd $PROJECT_NAME || { redecho "ERROR: Could not find repository folder: $PROJECT_NAME. Are you sure the configured repository exists"; exit 1; }
 git checkout $GIT_BRANCH
 
-$SRC_DIR/build.sh
+echo "Building app"
+$PWD/$SRC_DIR/build.sh
 
 while read p; do
     if [[ -d $p ]]; then
         cp -rf $p $INSTALL_DIR
     elif [[ -f $p ]]; then
         mv -f $p $INSTALL_DIR
+    else
+        if [[ "$p" =~ ^#.*  ]]; then
+	    continue
+	else
+            if [ -z "$p" ]; then
+                continue
+            else
+                echo "Warning: Not found $p"
+            fi           
+        fi
     fi
 done < $UPDATES_FILE
 
@@ -75,12 +86,23 @@ done < $UPDATES_FILE
 rm -rf $TEMP_DIR/$PROJECT_NAME
 
 echo
+
+UNINST_FILE=$INSTALL_DIR/uninstall.sh
+echo "Creating uninstaller"
+{
+  echo '#!/bin/bash'
+  echo 'if (( $EUID != 0 )); then'
+  echo '  echo "Please run as root!"'
+  echo "  exit 1"
+  echo "fi"
+} > $UNINST_FILE
+
 read -r -p "Enable command server at startup (N/y)? " OK
 OK=${OK:-n}
 if [ "$OK" == "y" ]; then
   cd /etc/systemd/system/ || { redecho "ERROR: Could not find /etc/systemd/system/. Are you sure this is a compatible platform?"; exit 1; }
 
-  # Create startup service
+  # Create startup service  
   SERVICE_FILE=cmdserver.service
   {
     echo "[Unit]"
@@ -99,5 +121,19 @@ if [ "$OK" == "y" ]; then
     echo WantedBy=multi-user.target
   } > $SERVICE_FILE
   systemctl enable $SERVICE_FILE
+
+  {
+    echo 'echo "disabling service"'
+    echo "systemctl disable $SERVICE_FILE"
+    echo "rm /etc/systemd/system/$SERVICE_FILE"
+  } >> $UNINST_FILE
+
 fi
+
+{
+  echo 'echo "Removing install dir"'
+  echo "rm -rf $INSTALL_DIR"
+} >> $UNINST_FILE
+chmod +x $UNINST_FILE
+
 
