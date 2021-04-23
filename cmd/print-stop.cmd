d074565f84b42e8d9df033b363e4c60aa44b00ac
Rada Berar <ujagaga@gmail.com>
2021-04-23 20:05:18 +0200

    Adapting stop script

--- cmd/print-stop.cmd
+++ cmd/print-stop.cmd
@@ -3,15 +3,17 @@
 # Stops the 3D printer
 import serial
 
+STOPMSG = "M25\nG28\nM18\nM104 S0\n"
+
 serial_dev = serial.Serial()
 serial_dev.port = "/dev/ttyUSB0"
 serial_dev.baudrate = 115200
 serial_dev.timeout = 0.5
 serial_dev.open()
 serial_dev.readline(100)       # Just emty the buffer
-serial_dev.write('M112\n'.encode())
+serial_dev.write(STOPMSG.encode())
 response_msg = serial_dev.readline(100).decode()
 serial_dev.close()
 
-print(response_msg.replace('\n', ''))
+print(response_msg.replace('\n', '|'))
 
