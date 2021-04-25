6b68bd28406c3767897cedd5af070e4f194c00e2
Rada Berar <ujagaga@gmail.com>
2021-04-25 09:27:33 +0200

    Updated printer script

--- cmd/print-stop.cmd
+++ cmd/print-stop.cmd
@@ -14,17 +14,33 @@
 
 import serial
 
-STOPMSG = "M25\nG1 E-2 Z0.2 F2400\nM106 S0\nM104 S0\nM140 S0\nM84 X Y E\nM18\nM300 S440 P200\nM300 S660 P250\nM300 S880 P300\n"
+UART = "/dev/ttyUSB0"
+
+UARTMSG = [
+    "M25",                  # Pause SD print
+    "G1 E-2 Z0.2 F2400",    # Retract and raise Z
+    "M106 S0",              # Turn-off fan
+    "M104 S0",              # Turn-off hotend
+    "M140 S0",              # Turn-off bed
+    "M84 X Y E",            # Disable all steppers but Z
+    "M18",                  # Disable steppers
+    "M300 S440 P200",       # Play sound
+    "M300 S660 P250",       # Play sound
+    "M300 S880 P300"        # Play sound
+]
 
 serial_dev = serial.Serial()
-serial_dev.port = "/dev/ttyUSB0"
+serial_dev.port = UART
 serial_dev.baudrate = 115200
-serial_dev.timeout = 0.5
+serial_dev.timeout = 0.1
 serial_dev.open()
-serial_dev.readline(100)       # Just emty the buffer
-serial_dev.write(STOPMSG.encode())
-response_msg = serial_dev.readline(100).decode()
+serial_dev.readline()       # Just empty the buffer
+serial_dev.timeout = 5
+
+for msg in UARTMSG:
+    outmsg = msg + '\n'
+    serial_dev.write(outmsg.encode())
+    response_msg = serial_dev.readline(100).decode()
+    print(response_msg.replace('\n', ''))
+
 serial_dev.close()
-
-print(response_msg.replace('\n', '|'))
-
