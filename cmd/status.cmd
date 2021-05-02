778dd97f0bcc1a72542f8c477f5763def7f2b6be
Rada Berar <ujagaga@gmail.com>
2021-05-02 11:15:17 +0200

    Add status script

---
+++ cmd/status.cmd
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Retrieves printer status
+
+import socket
+
+HOST = "localhost"
+PORT = 2000
+MSG_TIMEOUT = 10
+UARTMSG = [
+    "M27 C",
+    "M27 S4",
+]
+
+for msg in UARTMSG:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(msg + "\n", "utf-8"))
+        rx = sock.recv(1024)
+
+        print(rx.decode('utf-8'))
+
+
+
--- web/index.js
+++ web/index.js
@@ -17,11 +17,24 @@ function execute(cmd_name) {
     request.send();
 }
 
+function get_status() {
+    var request = new XMLHttpRequest();
+    request.open("GET", window.location.protocol + "//" + window.location.host + "/cmd/status");
+
+    request.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);          
+        }
+    };
+
+    request.send();
+}
+
 function setPreviewSize(){
 	var img = document.getElementById('preview');
 	
 	const width  = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) - 20;
-	const height = (window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight) - 60;
+	const height = (window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight) - 55;
 	
 	img.style.width = width + 'px';
 	img.style.height = 'auto';
@@ -36,4 +49,5 @@ window.onload = function() {
     var img = document.getElementById('preview');
     img.src = window.location.protocol + "//" + window.location.hostname + ":8080/?action=stream";
 	setPreviewSize();	
+    window.setInterval(get_status(), 2000);
 }
